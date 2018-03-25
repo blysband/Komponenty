37e68b4adff1eda7e5eeddf22837ffa76baa513b
Jakub Karpio <blysband@gmail.com>
2018-03-25 23:49:07 +0200

    pierwszy component

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-angular.module('myApp', [])
-.controller('studenciCtrl', function() {
-  this.studenci=[
-	{
-    imie: "Ala",
-    nazwisko: "Makota",
-    wiek:23,
-    oceny: [2,3,4,5]
-  },
-	{
-    imie: "Zosia",
-    nazwisko: "Samosia",
-    wiek:20,
-    oceny: [2,3,4]
-  },
-  {
-    imie: "Asia",
-    nazwisko: "Bezocen",
-    oceny: []
+myApp = angular.module('myApp', [])
+myApp.component('osoby',{
+  templateUrl: 'osoby.html',
+  controller: function studenciCtrl() {
+    this.studenci=[
+    {
+      imie: "Ala",
+      nazwisko: "Makota",
+      wiek:23,
+      oceny: [2,3,4,5]
+    },
+    {
+      imie: "Zosia",
+      nazwisko: "Samosia",
+      wiek:20,
+      oceny: [2,3,4]
+    },
+    {
+      imie: "Asia",
+      nazwisko: "Bezocen",
+      oceny: []
+    }
+    ];
+    this.average = function(student) {
+      let suma=0;
+        for(let i = 0;i<student.oceny.length;++i) {
+          suma += student.oceny[i];
+        }
+      return suma/student.oceny.length;
+    };
   }
-  ];
-  this.average = function(student) {
-    let suma=0;
-      for(let i = 0;i<student.oceny.length;++i) {
-        suma += student.oceny[i];
-      }
-    return suma/student.oceny.length;
-  };
+
 });
