3183f9a2f0fc7d16a734887601eba7773273ca90
Jakub Karpio <blysband@gmail.com>
2018-03-26 00:25:20 +0200

    komponent osob i pojedynczej osoby

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
-myApp = angular.module('myApp', [])
+myApp = angular.module('myApp', []);
+function studenciCtrl() {
+  this.studenci=[
+  {
+    imie: "Ala",
+    nazwisko: "Makota",
+    wiek:23,
+    oceny: [2,3,4,5]
+  },
+  {
+    imie: "Zosia",
+    nazwisko: "Samosia",
+    wiek:20,
+    oceny: [2,3,4]
+  },
+  {
+    imie: "Asia",
+    nazwisko: "Bezocen",
+    oceny: []
+  }
+  ];
+  this.average = function(student) {
+    let suma=0;
+      for(let i = 0;i<student.oceny.length;++i) {
+        suma += student.oceny[i];
+      }
+    return suma/student.oceny.length;
+  };
+};
 myApp.component('osoby',{
   templateUrl: 'osoby.html',
-  controller: function studenciCtrl() {
-    this.studenci=[
-    {
-      imie: "Ala",
-      nazwisko: "Makota",
-      wiek:23,
-      oceny: [2,3,4,5]
-    },
-    {
-      imie: "Zosia",
-      nazwisko: "Samosia",
-      wiek:20,
-      oceny: [2,3,4]
-    },
-    {
-      imie: "Asia",
-      nazwisko: "Bezocen",
-      oceny: []
-    }
-    ];
-    this.average = function(student) {
-      let suma=0;
-        for(let i = 0;i<student.oceny.length;++i) {
-          suma += student.oceny[i];
-        }
-      return suma/student.oceny.length;
-    };
-  }
+  controller: studenciCtrl
 
 });
+myApp.component('osoba',{
+  templateUrl: 'osoba.html',
+  bindings: {
+    student: '<'
+
+  }
+
+})
+;
